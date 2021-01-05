7f57ea0a0ec874974200961787ccf3dde58a463b
Purushottam <iDraciel@users.noreply.github.com>
2021-01-06 01:59:44 +0530

    changed js

--- Goals.js
+++ Goals.js
@@ -1,21 +1,19 @@
-var badgeIndex = 0;
-showBadge();
-function showBadge() {
-  var i;
-  var badge__slides = document.getElementsByClassName("myBadges");
-  var badge__dots = document.getElementsByClassName("badge__dot");
-if(window.innerWidth<600){
-    for (i = 0; i < badge__slides.length; i++) {
-      badge__slides[i].style.display = "none";
-    }
-    badgeIndex++;
-    if (badgeIndex > badge__slides.length) {badgeIndex = 1}
-    for (i = 0; i < badge__dots.length; i++) {
-      badge__dots[i].className = badge__dots[i].className.replace(" active", "");
-    }
-    badge__slides[badgeIndex-1].style.display = "block";
-    badge__dots[badgeIndex-1].className += " active";
-    setTimeout(showBadge, 2000);
-  }
+  var badgeIndex = 0;
+  showBadge();
+  function showBadge() {
+    var i;
+    var badge__slides = document.getElementsByClassName("myBadges");
+    var badge__dots = document.getElementsByClassName("badge__dot");
+      for (i = 0; i < badge__slides.length; i++) {
+        badge__slides[i].style.display = "none";
+      }
+      badgeIndex++;
+      if (badgeIndex > badge__slides.length) {badgeIndex = 1}
+      for (i = 0; i < badge__dots.length; i++) {
+        badge__dots[i].className = badge__dots[i].className.replace(" active", "");
+      }
+      badge__slides[badgeIndex-1].style.display = "block";
+      badge__dots[badgeIndex-1].className += " active";
+      setTimeout(showBadge, 2000);
 
-}
+  }
